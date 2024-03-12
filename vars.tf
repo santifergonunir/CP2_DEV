cdb8d16b1fce55b9d988368d50df1c00718ab11d
santifergonunir <163117988+santifergonunir@users.noreply.github.com>
2024-03-12 12:26:16 +0100

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  default = "cp2"
+}
+
+variable "location_name" {
+  default = "uksouth"
+}
+
+variable "network_name" {
+  default = "vnet1"
+}
+
+variable "subnet_name" {
+  default = "subnet1"
+}
+
+variable "usuario" {
+  default ="santiago.fernandez189@comunidadunir.net"
+}
+
+variable "password" {
+  default = "Buenosaires01"
+}
